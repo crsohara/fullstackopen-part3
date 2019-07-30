a8b77f9b3687bcd0e5122a3956e850322898b8e8
Chris O'Hara <crsohara@gmail.com>
2019-07-30 19:11:26 +0300

    Part3.17

--- models/person.js
+++ models/person.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
+mongoose.set('useFindAndModify', false)
+
 mongoose.connect(url, { useNewUrlParser: true })
   .then(result => {
     console.log('connected to MongoDB')
